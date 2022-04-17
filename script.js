1990027b85c644a583a31b7e7270e1be7d74ef4b
tanvirim <blackpanther141532@GMAIL.COM>
2022-04-17 16:39:08 +0600

    comment added and structured

--- script.js
+++ script.js
@@ -1,58 +1,59 @@
-const date = new Date();
 
-  const monthDays = document.querySelector(".days");
+// dynamic calendar 
 
-  const lastDay = new Date(date.getFullYear(),date.getMonth() + 1,0).getDate();
+
+
+  //variables
+
+   const date = new Date();
+
+   const lastDay = new Date(date.getFullYear(),date.getMonth() + 1,0).getDate();
+
+   const months = ["January","February","March","April","May","June","July","August","September","October","November","December",];
+
+   let monthDays = document.querySelector(".days");
+
+   // month and year print
  
+    document.querySelector(".date h1").innerHTML = months[date.getMonth()];
 
-  const lastDayIndex = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,0).getDay();
-
-
-  const months = ["January","February","March","April","May","June","July",
-  "August","September","October","November","December",];
-
-  document.querySelector(".date h1").innerHTML = months[date.getMonth()];
-
-  document.querySelector(".date p").innerHTML = new Date().getFullYear();
-
-  let days = "";
+    document.querySelector(".date p").innerHTML = new Date().getFullYear();
 
 
-  for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today"> ${i}</div>`;
+     //all days print and current day highlight
+
+    let days = "";
+
+    for (let i = 1; i <= lastDay; i++) {
+     if (
+        i === new Date().getDate() &&
+        date.getMonth() === new Date().getMonth()) {days += `<div class="today">${i}  </div>`;
       
-    } else {
-      days += `<div> ${i}</div>`;
-    }
+       } else {
+         days += `<div>${i}  </div>`;
+       }  
  
   }
-
- monthDays.innerHTML = days;
  
-
-
- select = document.querySelectorAll(".days div");
- console.log(select) ;
-select.forEach(function(elem) {
-  elem.addEventListener("click", function() {
+ monthDays.innerHTML = days;
    
-     let divValue = document.createElement("array") ;
-      divValue.innerHTML = elem.innerHTML+ "," ;
-      console.log(divValue.innerHTML) ;
-     newDiv =  document.getElementById("select") ;
-     newDiv.appendChild(divValue) ;
+//  date selection 
 
+ selectedDay = document.querySelectorAll(".days div");
+ 
+ selectedDay.forEach(function(elem) {
+  
+  elem.addEventListener("click", function() {
+
+     let dateArray = document.createElement("array") ;
+
+    dateArray.innerHTML = elem.innerHTML+ "," ;
+    
+    dateDiv =  document.getElementById("select") ;
+
+    dateDiv.appendChild(dateArray) ;
+       
   });
+  
 });
-
-
-
-
-
 
